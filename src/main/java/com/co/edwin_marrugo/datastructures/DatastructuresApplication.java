56d612c5b7254db18e5c20eb78318c07f6ac5995
Edwin Marrugo <edwinmarr@hotmail.com>
2023-12-23 00:35:00 -0500

    first commit

---
+++ src/main/java/com/co/edwin_marrugo/datastructures/DatastructuresApplication.java
@@ -0,0 +1,13 @@
+package com.co.edwin_marrugo.datastructures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatastructuresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatastructuresApplication.class, args);
+	}
+
+}
